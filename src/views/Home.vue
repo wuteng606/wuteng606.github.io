0bbb8876d60e8abb5ea3e32e0d631cf6823ef934
wuteng606 <wuteng606@163.com>
2019-03-29 14:23:20 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<a @click="getData">test</a>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+
+export default {
+	name: "home",
+	components: {
+		HelloWorld
+	},
+	methods: {
+		getData() {
+            console.log("click");
+		}
+	}
+};
+</script>
